b2c27b4904b59405c1698ca104ce200f1b0840f3
Duarte Golaio Gonçalves <>
2025-03-14 15:37:01 +0000

    Changes: First Commit Base Project with imports

---
+++ Lbc_test_album_titles/app/src/main/java/com/example/lbc_test_album_titles/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.lbc_test_album_titles
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.lbc_test_album_titles.ui.theme.Lbc_test_album_titlesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Lbc_test_album_titlesTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Lbc_test_album_titlesTheme {
+        Greeting("Android")
+    }
+}
