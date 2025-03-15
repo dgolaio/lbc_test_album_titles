fccbc0ad4a16d12b5bc7da5d3b85dab156328c43
Duarte Golaio Gonçalves <>
2025-03-15 12:51:25 +0000

    changes: -Added Screen and composables with Multiple requests - Added Coil Library to Present Images with URL provided

---
+++ Lbc_test_album_titles/app/src/main/java/com/example/lbc_test_album_titles/compose/BaseScreen.kt
@@ -0,0 +1,26 @@
+package com.example.lbc_test_album_titles.compose
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.lbc_test_album_titles.viewmodel.AlbumTitlesViewModel
+
+@Composable
+fun BaseScreen(modifier: Modifier = Modifier,  viewModel: AlbumTitlesViewModel = hiltViewModel()) {
+    val albumItems by viewModel.albumItems.collectAsState()
+
+    LazyColumn (modifier = modifier
+        .fillMaxSize()
+        .background(Color.LightGray)
+    ) {
+        items(albumItems.size) { albumPos ->
+            AlbumItemCompose(albumTitle = albumItems.get(albumPos).title, imageUrl = albumItems.get(albumPos).imageUrl)
+        }
+    }
+}
