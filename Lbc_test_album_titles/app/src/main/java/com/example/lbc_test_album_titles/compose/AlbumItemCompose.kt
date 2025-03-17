822a6cd62cdf981581d1ef8c2cc6ef61ff02b0a9
Duarte Golaio Gonçalves <>
2025-03-17 10:43:54 +0000

    changes: added darkMode config Change

--- Lbc_test_album_titles/app/src/main/java/com/example/lbc_test_album_titles/compose/AlbumItemCompose.kt
+++ Lbc_test_album_titles/app/src/main/java/com/example/lbc_test_album_titles/compose/AlbumItemCompose.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -25,7 +26,7 @@ import coil.compose.AsyncImage
 fun AlbumItemCompose(modifier: Modifier = Modifier, albumTitle: String, imageUrl:String) {
     Box(modifier = modifier
         .fillMaxWidth(1f)
-        .border(BorderStroke(1.dp, Color.DarkGray))
+        .border(BorderStroke(1.dp, MaterialTheme.colorScheme.secondary))
         .padding(20.dp)
         ,contentAlignment = Alignment.Center) {
         Column(modifier = Modifier.fillMaxHeight(),
@@ -35,12 +36,19 @@ fun AlbumItemCompose(modifier: Modifier = Modifier, albumTitle: String, imageUrl
                 .fillMaxWidth(1f)
                 .background(Color.DarkGray)
             ){
-                Text(modifier = Modifier, text = albumTitle, textAlign = TextAlign.Center, fontSize = 24.sp, color = Color.White, fontFamily = FontFamily.Monospace)
+                Text(modifier = Modifier,
+                     text = albumTitle,
+                     textAlign = TextAlign.Center,
+                     fontSize = 24.sp,
+                     color = Color.White,
+                     fontFamily = FontFamily.Monospace)
             }
 
             Spacer(modifier = Modifier.height(20.dp))
-            Box(modifier = Modifier.border(BorderStroke(2.dp, Color.DarkGray))){
-                AsyncImage(model = imageUrl, contentDescription = "$albumTitle + Image", alignment = Alignment.Center)
+            Box(modifier = Modifier.border(BorderStroke(2.dp, MaterialTheme.colorScheme.secondary))){
+                AsyncImage(model = imageUrl,
+                            contentDescription = "$albumTitle + Image",
+                            alignment = Alignment.Center)
             }
         }
     }
